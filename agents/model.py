588f058af79cadcdaf622315423d7cc794495253
quantumiracle <zhding@mail.ustc.edu.cn>
2023-03-07 15:27:33 -0500

    Revert "add consistency"
    This reverts commit 9259a18ac578b585a44a4c04d6a0846044db20d1.

--- agents/model.py
+++ agents/model.py
@@ -2,7 +2,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import numpy as np
-import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -41,94 +40,11 @@ class MLP(nn.Module):
         self.final_layer = nn.Linear(256, action_dim)
 
     def forward(self, x, time, state):
-        time = time.squeeze(1)  # added for shaping t from (batch_size, 1) to (batch_size,)
+
         t = self.time_mlp(time)
         x = torch.cat([x, t, state], dim=1)
         x = self.mid_layer(x)
 
         return self.final_layer(x)
 
-
-blk = lambda ic, oc: nn.Sequential(
-    nn.GroupNorm(32, num_channels=ic),
-    nn.SiLU(),
-    nn.Conv2d(ic, oc, 3, padding=1),
-    nn.GroupNorm(32, num_channels=oc),
-    nn.SiLU(),
-    nn.Conv2d(oc, oc, 3, padding=1),
-)
-
-class Unet(nn.Module):
-    def __init__(self, 
-        n_channel: int,
-        D: int = 128,
-        device: torch.device = torch.device("cpu"),
-        ) -> None:
-        super(Unet, self).__init__()
-        self.device = device
-
-        self.freqs = torch.exp(
-            -math.log(10000) * torch.arange(start=0, end=D, dtype=torch.float32) / D
-        )
-
-        self.down = nn.Sequential(
-            *[
-                nn.Conv2d(n_channel, D, 3, padding=1),
-                blk(D, D),
-                blk(D, 2 * D),
-                blk(2 * D, 2 * D),
-            ]
-        )
-
-        self.time_downs = nn.Sequential(
-            nn.Linear(2 * D, D),
-            nn.Linear(2 * D, D),
-            nn.Linear(2 * D, 2 * D),
-            nn.Linear(2 * D, 2 * D),
-        )
-
-        self.mid = blk(2 * D, 2 * D)
-
-        self.up = nn.Sequential(
-            *[
-                blk(2 * D, 2 * D),
-                blk(2 * 2 * D, D),
-                blk(D, D),
-                nn.Conv2d(2 * D, 2 * D, 3, padding=1),
-            ]
-        )
-        self.last = nn.Conv2d(2 * D + n_channel, n_channel, 3, padding=1)
-
-    def forward(self, x, t) -> torch.Tensor:
-        # time embedding
-        args = t.float() * self.freqs[None].to(t.device)
-        t_emb = torch.cat([torch.sin(args), torch.cos(args)], dim=-1).to(x.device)
-
-        x_ori = x
-
-        # perform F(x, t)
-        hs = []
-        for idx, layer in enumerate(self.down):
-            if idx % 2 == 1:
-                x = layer(x) + x
-            else:
-                x = layer(x)
-                x = F.interpolate(x, scale_factor=0.5)
-                hs.append(x)
-
-            x = x + self.time_downs[idx](t_emb)[:, :, None, None]
-
-        x = self.mid(x)
-
-        for idx, layer in enumerate(self.up):
-            if idx % 2 == 0:
-                x = layer(x) + x
-            else:
-                x = torch.cat([x, hs.pop()], dim=1)
-                x = F.interpolate(x, scale_factor=2, mode="nearest")
-                x = layer(x)
-
-        x = self.last(torch.cat([x, x_ori], dim=1))
-
-        return x
 
